725c5738da941b32e6b2cfe8929907064b8d8e2f
root <root@me>
2014-02-15 01:09:17 -0600

    tmp

--- git/crash_specifier.py
+++ git/crash_specifier.py
@@ -1,87 +1,107 @@
 import datetime
 load(0)
 
-if True:
-	load(0)
-	R = dops_double(128)
-	E = (37,5)
-	dops_end_at(E)
-	dops_omit(R)
-	dops_replay()
+# NOTE: There is one important distinction between the remove() calls and the
+# omit() calls. While removing, you actually end up changing the index number
+# of every disk_op/micro_op after the current one. Omitting leaves the index
+# numbers as such. Thus, "dops_remove(31); dops_remove(31); dops_remove(31);"
+# removes three separate disk operations. However, "dops_omit(31);
+# dops_omit(31); dops_omit(31);" omits only one oepration.
 
-if False:
-	all_combos = []
-	combos_count = 0
+def prefix_run():
 	load(0)
 	for i in range(0, dops_len()):
-		drop_set = [dops_double(i)]
-		for j in range(i + 1, dops_len()):
-			drop_set.append(dops_double(j))
-			till = dops_independent_till(drop_set)
-			print('Combos count: ' + str((dops_double(i), dops_double(j))) + ' ' + str(combos_count) + ' ' + str(till))
-			till = dops_single(till)
-			if till < j:
-				break
-			combos_count += (till - j)
-			for k in range(j + 1, till + 1):
-				all_combos.append((i, j, k))
+		E = str(i) + str(dops_double(i))
+		dops_end_at(dops_double(i))
+		dops_replay(str(datetime.datetime.now()) +
+				' E' + E)
 
-	print 'Done getting all_combos.'
-	print combos_count
-	all_combos.reverse()
+def omit_one_heuristic():
+	load(0)
+	last = None # Just used for asserting that the algorithm is correct
 
-if False:
-	last = 0
 	for i in range(0, dops_len()):
 		load(0)
+
 		till = dops_single(dops_independent_till(dops_double(i)))
+		# 'till' now contains the index of the last disk_op, till which
+		# execution can continue legally, while omitting (i.e., still
+		# buffering in memory) the 'i'th disk_op.
+
 		for j in range(i + 1, till + 1):
 			load(0)
-			#assert (i, j) == all_combos.pop()
 			R = str(i) + str(dops_double(i))
 			E = str(j) + str(dops_double(j))
 			end_at(dops_double(j))
-			dops_remove(dops_double(i))
+			dops_omit(dops_double(i))
 			last = (i, j)
 			dops_replay(str(datetime.datetime.now()) +
 						' R' + R +
 						' E' + E)
-	load(0)
-	print last
-	print (dops_len() - 2, dops_len() - 1)
+			load(0) # This is actually the only load(0) required
+				# inside any of the for loops. The others are there just for readability.
+
 	assert last == (dops_len() - 2, dops_len() - 1)
 
-if False:
+def omit_range_heuristic():
 	load(0)
+
+	# 'i' is the beginning of the range to be omitted.
 	for i in range(0, dops_len()):
+
+		# 'drop_set' contains the set of operations that are to be
+		# omitted. i.e., all operations which fall within the range.
 		drop_set = [dops_double(i)]
+
+		# 'j' is the end of the range to be omitted.
 		for j in range(i + 1, dops_len()):
 			drop_set.append(dops_double(j))
 			till = dops_single(dops_independent_till(drop_set))
+			
 			if till < j:
 				break
+
+			# 'k' is the disk_op till which the trace is to be
+			# replayed after omitting the range previously decided.
 			for k in range(j + 1, till + 1):
-				#assert (i, j, k) == all_combos.pop()
 				R = str(i) + str(dops_double(i)) + '...' + str(j) + str(dops_double(j))
 				E = str(k) + str(dops_double(k))
+
+				# end at k
 				dops_end_at(dops_double(k))
-				for drop_op in reversed(drop_set): dops_omit(drop_op)
+
+				# omit everything in the drop set
+				for drop_op in drop_set: dops_omit(drop_op)
+
 				dops_replay(str(datetime.datetime.now()) +
 							' R' + R +
 							' E' + E)
 				load(0)
 
-print 'Done crash specification.'
-#_dops_verify_replayer()
-#auto_test(limit = 10)
-#for i in range(0, 11):
-#	load(0)
-#	end_at(i)
-#	replay_and_check()
-#for i in range(6,11):
-#	load(0)
-#	#end_at(i)
-#	remove(i)
-#	replay_and_check(i)
-#auto_test(limit=10)
-#export_pickle('/tmp/a')
+def example_calls():
+	load(0)
+
+	# set_garbage(3)
+	# Set the micro-op of index 3 as garbage. (It should
+	# be a write or append.) This will work, but I don't see why we'd be
+	# using this now, as will replay_and_check() that replays using
+	# micro-ops. Also, if this call is used, dops_generate() should be used
+	# to generate the corresponding disk-ops.
+
+	dops_generate(splits = 4) # Generate disk ops from micro op, splitting each append, write, and truncate micro-op into four disk ops
+	dops_generate(4, splits = 10) # Split micro-op with index 4 into ten disk ops 
+	dops_generate([8, 9, 10], splits = 1) # Generate disk ops from the micro-ops with index 8, 9, and 10, without any splitting
+	save(1)
+	dops_set_legal() # Tells the framework that the current displayed set of disk ops is the one that should be considered correct/legal.
+
+
+	load(1)
+	dops_omit(dops_double(1))
+	dops_end_at(dops_double(2))
+	dops_replay()
+
+load(0)
+for i in range(52, 60):
+	dops_end_at(dops_double(i))
+	dops_replay()
+#prefix_run()
